662c3c25d7312ebaa9bb093f503f4b66f511bb55
HuXufeng <huxufeng0120@gmail.com>
2019-01-05 21:55:13 +0800

    这次一定能成功了!

--- static/js/type.js
+++ static/js/type.js
@@ -1,7 +1,7 @@
 /*测试用户是否有此字体 test if user have certain type of font*/
-let isSupportFontFamily=function(f){if(typeof f!="string"){return false}var h="Arial";if(f.toLowerCase()==h.toLowerCase()){return true}var e="a";var d=100;var a=100,i=100;var c=document.createElement("canvas");var b=c.getContext("2d");c.width=a;c.height=i;b.textAlign="center";b.fillStyle="black";b.textBaseline="middle";var g=function(j){b.clearRect(0,0,a,i);b.font=d+"px "+j+", "+h;b.fillText(e,a/2,i/2);var k=b.getImageData(0,0,a,i).data;return[].slice.call(k).filter(function(l){return l!=0})};return g(h).join("")!==g(f).join("")};
+var isSupportFontFamily=function(f){if(typeof f!="string"){return false}var h="Arial";if(f.toLowerCase()==h.toLowerCase()){return true}var e="a";var d=100;var a=100,i=100;var c=document.createElement("canvas");var b=c.getContext("2d");c.width=a;c.height=i;b.textAlign="center";b.fillStyle="black";b.textBaseline="middle";var g=function(j){b.clearRect(0,0,a,i);b.font=d+"px "+j+", "+h;b.fillText(e,a/2,i/2);var k=b.getImageData(0,0,a,i).data;return[].slice.call(k).filter(function(l){return l!=0})};return g(h).join("")!==g(f).join("")};
 
-if(isSupportFontFamily('Source Han Serif CN')||isSupportFontFamily("Source Han Serif SC")||isSupportFontFamily("Noto Serif CJK SC"){
+if(isSupportFontFamily('Source Han Serif CN')||isSupportFontFamily("Source Han Serif SC")||isSupportFontFamily("Noto Serif CJK SC")){
     void(0);
 }
 else {
