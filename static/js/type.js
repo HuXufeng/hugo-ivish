e041be93da2cbdcf83244b62ce7592c3920c68f4
HuXufeng <huxufeng0120@gmail.com>
2019-01-09 00:29:03 +0800

    dd

---
+++ static/js/type.js
@@ -0,0 +1,15 @@
+(function(d) {
+    // https://www.kirupa.com/html5/detect_whether_font_is_installed.htm
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext("2d");
+    var text = "abcdefghijklmnopqrstuvwxyz0123456789";
+    context.font = "72px monospace";
+    var size = context.measureText(text).width;
+    var fonts = [' SC', ' CN', ' TC', ' TW', ''];
+    for (var i = 0; i < fonts.length; i++) {
+      context.font = "72px '" + 'Source Han Serif' + fonts[i] + "', monospace";
+      // no need to load TypeKit if Source Hans Serif has been installed
+      if (context.measureText(text).width != size) return;
+    }
+    var link = document.createElement("link");link.rel = "stylesheet";link.href = "https://fonts.googleapis.com/css?family=Noto+Serif+SC:400,700";document.getElementsByTagName("head")[1].appendChild(link);
+  })(document);
